1210830dc3563b0e97fd6fc487f18fd6eeaf46ce
Kushagra Singhal <singhalkushagra21@gmail.com>
2025-06-26 02:14:05 +0530

    fixing

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 
-const API_BASE_URL = 'https://go-practice-projects-1.onrender.com/';
+const API_BASE_URL = 'https://go-practice-projects-1.onrender.com';
 
 export interface Todo {
   id?: string;
